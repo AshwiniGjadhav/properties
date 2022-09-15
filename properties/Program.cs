1c7c2b364a18a82ad5cef5dc93ecac25b0918ffd
Ashwini <Ashwini@LAPTOP-FQL7UM83>
2022-09-15 17:50:59 +0530

    Add project files.

---
+++ properties/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student { Rollno = 2,Name = "viraj",Percentage = 58};
+            Student s1 = new Student ();
+
+             s1.Rollno = 1;
+             s1.Name = "onkar";     
+              s1.Percentage = 56;
+
+            //Console.WriteLine($"details {1.Rollno} {s1.Name} {s1.Percentage}");
+            Console.WriteLine(s);
+            Console.WriteLine(s1);  
+        }
+    }
+}
---
+++ properties/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    internal class Student
+    {
+        private int rollno;
+        private string name;
+        private int percentage;
+
+        public int Rollno
+        {
+            get { return rollno; }
+            set { rollno = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Percentage
+        {
+            get { return percentage; }
+            set { percentage = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"student detail: {rollno} {name} { percentage}";
+        }
+    }
+}
